31fe130b6270b81b3cdfc3e40511a79ac77b9930
Linlin <hanlinlin@live.com>
2017-07-11 15:27:47 -0400

    Added Human Abstract class

---
+++ COMP123 S2017 - Lesson9A/Human.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+/* Name:
+ * Date: 
+ * Description:
+ * Version: 0.1-class created
+ */ 
+ 
+namespace COMP123_S2017___Lesson9A
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public abstract class Human
+    {
+        //PRIVATE FIELDS
+        private string _name;
+
+        //propertirs
+        public string Name {
+            get
+            {
+                return this._name;
+            }
+
+            set
+            {
+                this._name = value;
+            }
+
+        }
+            // CONS
+        public Human(string name)
+        {
+            ///
+            this.Name = name;
+        } 
+    }
+}

a22fccf239e59993717fbbf84ecead323d424dcd
Linlin <hanlinlin@live.com>
2017-07-11 15:42:08 -0400

    Added SuperHuman subclass

---
+++ COMP123 S2017 - Lesson9A/SuperHuman.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+/*
+ * Name:
+ * Date:
+ * 
+ */
+ 
+namespace COMP123_S2017___Lesson9A
+{
+    /// <summary>
+    /// This is the SuperHuman subclass.
+    /// 
+    /// </summary>
+    public class SuperHuman: Human
+    {
+        // Private field
+
+        //public properties
+
+        //constructors
+        public SuperHuman(string name)
+            : base(name)
+        {
+
+        }
+
+        //public  methods
+    }
+}
